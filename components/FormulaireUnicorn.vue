da244031ff1fddaa869ee4fc0e97d25e76aafed8
Kevin Hournont <kevin.hournont@symbol-it.fr>
2022-04-12 16:50:41 +0200

    :sparkles: update capacite licorne

--- components/FormulaireUnicorn.vue
+++ components/FormulaireUnicorn.vue
@@ -43,6 +43,7 @@
                 class="formulaire-unicorn__input"> 
 
             <div class="formulaire-unicorn__capacities">
+                <label class="formulaire-unicorn__label--display"> Capacités </label>
                 <div class="formulaire-unicorn__capacities__items" v-for="(cap,index) in capacities" :key="index"> 
                     <input type="checkbox" class="formulaire-unicorn__capacities__items__input" :id="cap.label" :value="cap" v-model="form.capacities"> 
                     <label :for="cap.label" class="formulaire-unicorn__capacities__items__label"> {{ cap.label }} </label>
@@ -129,38 +130,7 @@ export default{
     padding: 10px 20px;
     margin: 5px 5px 10px 5px;
 }
-.formulaire-unicorn__span-capacities{
-    display:block;
-}
 
-.formulaire-unicorn__span-capacities__label{
-    text-align: center;
-    display: inline-block;
-    width: 150px;
-    margin: 5px 0 5px 0;
-    padding: 15px 0 15px 0;
-    border-radius: 16px;
-    cursor: pointer;
-    color: white;
-}
-
-.formulaire-unicorn__span-capacities__label:hover{
-    box-shadow: inset 0px -4px 0px rgba(255, 255, 255, 0.25);
-    transition: box-shadow 0.5s;
-}
-
-.formulaire-unicorn__span-capacities__input:checked + .formulaire-unicorn__span-capacities__label{
-    background-color: green;
-}
-
-
-.formulaire-unicorn__span-capacities__input{
-    display: none;
-}
-
-.formulaire-unicorn__span-capacities__input + .formulaire-unicorn__span-capacities__label{
-    background-color: red;
-}
 
 .formulaire-unicorn__capacities__items__label{
     text-align: center;
@@ -180,6 +150,7 @@ export default{
 
 .formulaire-unicorn__capacities__items__input:checked + .formulaire-unicorn__capacities__items__label{
     background-color: green;
+    border: 0;
 }
 
 
@@ -188,7 +159,9 @@ export default{
 }
 
 .formulaire-unicorn__capacities__items__input + .formulaire-unicorn__capacities__items__label{
-    background-color: red;
+    background-color: white;
+    color: black;
+    border: 2px solid #7EA8EF;
 }
 
 hr {
